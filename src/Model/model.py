d28cc6e6753db5190f14dc3b8f892078caba6e13
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2026-06-30 19:51:22 -0300

    feat: add def classificar_imc  no arquivo model.py

--- src/Model/model.py
+++ src/Model/model.py
@@ -12,6 +12,20 @@ class UsuarioModel:
         imc = self.peso / (altura_usuario ** 2)
         return imc
     
+    def classificar_imc(self):
+            imc = self.calcular_imc()
+            if imc < 18.5:  
+                return "Abaixo do peso"
+            elif 18.5 <= imc < 24.9:
+                return "Peso normal"
+            elif 25 <= imc < 29.9:
+                return " Acima do peso (Sobrepeso)"
+            elif 30 <= imc < 34.9:
+                return "Obesidade grau I"
+            elif 35 <= imc < 39.9:
+                return "Obesidade grau II (Severa)"
+            else:
+                return "Obesidade grau III (Mórbida)"
     
         
   
